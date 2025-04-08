e84d1b415626e51b4765de7de057894e95a990d6
gianfrancodepaula <gianfranco.depaula@dockmaster.com>
2025-04-08 16:26:57 -0400

    Add password complexity validation and UUID extension to migrations

--- internal/requests/auth.go
+++ internal/requests/auth.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 type BasicAuth struct {
 	Email    string `json:"email" validate:"required,email" example:"john.doe@example.com"`
-	Password string `json:"password" validate:"required,min=8" example:"Pa$$w0rd123"`
+	Password string `json:"password" validate:"required,min=12,letters,number,specialchar" example:"Pa$$w0rd123"`
 }
 
 type LoginRequest struct {
