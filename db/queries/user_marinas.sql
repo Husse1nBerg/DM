2a2b29b67d7675be28557a748bafa32bc3905630
gianfrancodepaula <gianfranco.depaula@dockmaster.com>
2025-07-03 10:40:06 -0400

    DM-255 Refactor GetUsersByMarinaHandler to use user_marinas table for filtering and enhance pagination logic. It will return the external and external users list

--- db/queries/user_marinas.sql
+++ db/queries/user_marinas.sql
@@ -94,3 +94,13 @@ WHERE u.deleted_at IS NULL
     WHERE um.user_id = u.id
       AND um.marina_id = $1
   );
+-- name: ListUserMarinasAssignmentsPaginated :many
+SELECT um.*, u.*, r.name as role_name
+FROM user_marinas um
+JOIN users u ON u.id = um.user_id
+LEFT JOIN roles r ON u.role_id = r.id
+WHERE um.marina_id = $1
+  AND ($2::bool IS NULL OR ($2 = TRUE AND um.customer_id IS NOT NULL) OR ($2 = FALSE AND um.customer_id IS NULL))
+  AND u.deleted_at IS NULL
+ORDER BY u.created_at DESC
+LIMIT $3 OFFSET $4;
